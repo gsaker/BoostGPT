7c71b699a419db2d90adad07c092261839deee83
Your Name <you@example.com>
2023-03-17 18:44:51 +0000

    new

--- gui copy.py
+++ gui copy.py
@@ -1,6 +1,6 @@
 # Import PySide2 module
-from PySide2 import *
-from PySide2.QtWidgets import *
+from PyQt6 import *
+from PyQt6.QtWidgets import *
 from revChatGPT.V3 import Chatbot
 
 import threading
@@ -8,7 +8,8 @@ import threading
 chatbot = Chatbot(api_key="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")
 # Create an application instance
 app = QApplication([])
-
+responseMessage=""
+newWord=True
 # Create a main window
 window = QWidget()
 
@@ -24,15 +25,29 @@ text_edit.setReadOnly(True)
 # Create a submit button to trigger the function
 button = QPushButton("Submit")
 def getInfo(prompt):
+    global responseMessage
+    global newWord
+    responseMessage = ""
     print(prompt)
     for data in chatbot.ask_stream(prompt):
-        text_edit.insertPlainText(data)
-
+        newWord = True
+        print("data",data)
+        responseMessage=data
+def runGUI():
+    while True:
+        QApplication.processEvents()
 # Define a function to handle submit button click
 def submit():
+    global newWord
     # Get the input from the line edit
     getThread = threading.Thread(target=getInfo,args=(line_edit.text(),))
     getThread.start()
+    while (getThread.is_alive()):
+        if (newWord==True):
+            #QApplication.processEvents()
+            #print("J")
+            text_edit.insertPlainText(responseMessage)
+            newWord = False
 
 # Connect the button to the function
 button.clicked.connect(submit)
@@ -50,7 +65,7 @@ window.setLayout(layout)
 window.show()
 # Start the main loop
 
-app.exec_()
+app.exec()
 
 
 
